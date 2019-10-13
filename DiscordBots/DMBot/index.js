daeba28c96f7f07d7d85df90c2ac1ecfb1ad835c
Joseph Adams <jadam090@fiu.edu>
2019-10-13 02:35:13 -0400

    Created bot that can read text starting with >>

---
+++ DiscordBots/DMBot/index.js
@@ -0,0 +1,24 @@
+const Discord = require('discord.js');              //create a variable (using kw 'const') to get node modules from discord.js
+const { prefix, token} = require('./config.json');  //puts properties from config so that you can use the values defined there
+const client = new Discord.Client();                
+const dummyVal = -777;
+
+//boot msg
+client.once('ready', () => {
+    console.log('DM is ready!')
+})
+
+//reads messages people put into text channels
+client.on('message', message => {
+    //console.log(message.content);   //just prints it to the console
+
+    if(message.content.startsWith(prefix + "msg")) {
+        message.channel.send("printing generic test message");
+    }
+    else if (message.content.startsWith(prefix + "roll")) {
+        message.channel.send("you rolled: " + dummyVal);
+    }
+})
+
+//actually logging in; can use token property instead of inputting it here; safer
+client.login(token);    
