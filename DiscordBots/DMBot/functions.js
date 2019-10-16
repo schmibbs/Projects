49dc8129b6618dd5bbb0bf3c6713799b5b83b5ad
Joseph Adams <jadam090@fiu.edu>
2019-10-15 23:51:32 -0400

    Added test function for RNGenerator and a stub for a storage method

--- DiscordBots/DMBot/functions.js
+++ DiscordBots/DMBot/functions.js
@@ -1,4 +1,22 @@
-function randNum(upperLim) {
-    return Math.random() * upperLim;
-}
+class Functions {
+    //constructor
+    Functions(name) {
+        this.name = name;
+    }
 
+    //generates a positive random number given some upper limit
+    randNum(upperLim) {
+        return Math.random() * upperLim;
+    }
+
+    //test to see which Math function to apply to random result
+    randNumTest(upperLim) {
+        console.log('RES using floor(): ' + Math.floor(this.randNum(upperLim)));
+        console.log('RES using ceiling(): ' + Math.ceil(this.randNum(upperLim)));
+    }
+
+    //used to keep progress from previous games
+    storeCard() {
+        //look into using json or some other storage type
+    }
+}
